60b313ab878d54db83246d5aec00eb2b190838e5
Eric Halsey <ehalsey@a-cto.com>
2021-03-12 14:31:16 -0800

    not working embed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const pdf = <embed type='application/pdf' width="100%" height="99.5%" src="invoice.pdf" />;
+  return (
+    <div class="container">
+      <div class="row headerRow">
+        <div class="col align-self-start titleCol">
+          Bamert Seed Company
+      </div>
+        <div class="col-4 agingcontainer align-self-end">
+          <div class="agingCol aging01Col">20</div>
+          <div class="agingCol aging02Col">30</div>
+          <div class="agingCol aging03Col">40</div>
+          <div class="agingCol aging04Col">50</div>
+          <div class="agingCol aging05Col">6</div>
+        </div>
+      </div>
+      <div class="row" style={{ minHeight: "600px" }}>
+        <div class="col-sm-8 pdfCol" style={{ padding: "0px" }}>
+          {pdf}
+        </div>
+        <div class="col-sm formCol">
+          form
+      </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
